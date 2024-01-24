60350ed2c2b7b38ad98e56e753dfd178f95b81e4
Seungyeon Chang <schang10@una.edu>
2024-01-23 23:24:24 -0600

    another thing

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5500;
 
 app.get('/', function (req, res) {
     
   res.send('Hello World from Express')
 })
 
-app.listen(3000)
+app.get('/', function (req, res) {
+    
+  res.sendFile(__dirname+'/index.html')
+})
+
+//app.listen(3000)
+
+app.listen(port, () => {
+  console.log(`mike app listening on port ${port}`)
+})
