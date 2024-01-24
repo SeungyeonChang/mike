a90735e38cf88bde65d2d9ef322e4f6954d97f43
Seungyeon Chang <schang10@una.edu>
2024-01-24 16:51:35 -0600

    tada!

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
 
-app.get('/', function (req, res) {
+// app.get('/', function (req, res) {
     
-  res.send('Hello World from Express')
-})
+//   res.send('Hello World from Express')
+// })
 
 app.get('/', function (req, res) {
     
