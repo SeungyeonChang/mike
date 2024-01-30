13aa8805a322c3f1740480b9cceecd8cba8d87de
Seungyeon Chang <schang10@una.edu>
2024-01-30 15:30:55 -0600

    new one

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5500;
-
+const port = process.env.PORT || 5000;
+//set the view engine to ejs
+let path = require('path');
+app.set('views',path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 // app.get('/', function (req, res) {
     
 //   res.send('Hello World from Express')
 // })
 
-app.get('/', function (req, res) {
+// user res.render to load up an ejs view file
+
+let myTypeServer ="The Investigator";
+
+app.get('/', function (req, res){
+  res.render('index', {
+    'myTypeClient': myTypeServer
+  })
+});
+
+
+app.get('/send', function (req, res) {
     
   res.sendFile(__dirname+'/index.html')
 })
@@ -15,5 +29,5 @@ app.get('/', function (req, res) {
 //app.listen(3000)
 
 app.listen(port, () => {
-  console.log(`mike app listening on port ${port}`)
+  console.log(`nov app listening on port ${port}`)
 })
